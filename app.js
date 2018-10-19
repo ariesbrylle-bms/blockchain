d092227520345ec3259e6ab2eb66e9ff68c7362c
Aries Brylle Ventura <aries@Ariess-MacBook-Air.local>
2018-10-19 16:56:40 +0800

    From branch 1

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 
+var dogs = []
+
 app.get('/', (req, res) =>
     res.status(200).json({
         greeting: 'Hello World!'
@@ -11,6 +13,16 @@ app.get('/', (req, res) =>
 app.post('/', (req, res) =>
     res.status(200).json({
         greeting: `Hello ${req.body.username}!`
+    }));
+
+app.post('/dog', (req, res) => {
+    dogs.push(req.body.dogname);
+    return res.status(200).json({ greeting: `Hello ${req.body.dogname}!` });
+})
+
+app.get('/dog', (req, res) =>
+    res.status(200).json({
+        dogs: `${dogs}`
     }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
